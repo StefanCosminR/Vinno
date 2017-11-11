2701bb83eefe8333df5f263de98b3d7776531a37
stefan-cosmin <steancosmin.romanescu@gmail.com>
2017-11-11 13:22:57 +0200

    Background now send to content scripts the annotator template

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -2,9 +2,23 @@
  * @description here we map routes where we want the extension to be clickable
  */
 
-chrome.tabs.onUpdated.addListener(function(tab_id, data, tab) {
-    if(tab.url.indexOf("stackoverflow") !== -1) {
-        chrome.pageAction.show(tab_id);
+chrome.tabs.onUpdated.addListener(function (tab_id, data, tab) {
+    if (tab && tab.url) {
+        if (tab.url.indexOf("stackoverflow") !== -1) {
+            chrome.pageAction.show(tab_id);
+        }
     }
-
 });
+
+/**
+ * @todo Maybe the request should be done asynchronously in the future, but for now synchronous request does not affect performance notably
+ */
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request === "getEmbeddedHtml") {
+            let xmlHttp = new XMLHttpRequest();
+            xmlHttp.open('GET', "../src/html/annotatorTemplate.html", false);
+            xmlHttp.send(null);
+            sendResponse({html: xmlHttp.responseText});
+        }
+    });
