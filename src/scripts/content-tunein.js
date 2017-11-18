f5429c167a4771785f6549d9cca39e18e3c80582
stefan-cosmin <steancosmin.romanescu@gmail.com>
2017-11-18 15:47:59 +0200

    Changes content-tunein to use getAllDependencies instead of manually getting them

--- src/scripts/content-tunein.js
+++ src/scripts/content-tunein.js
@@ -1,139 +1,140 @@
-chrome.runtime.sendMessage("getEmbeddedHtml", function (response) {
+getAllDependencies()
+	.then(dependencies => {
+        function main_function()
+        {
+            show_added_annotations_until_now();
 
-	function main_function() 
-	{
-		show_added_annotations_until_now();
+            var all_annotations_moments = []
+            var all_annotations_comments = []
 
-	    var all_annotations_moments = []
-	    var all_annotations_comments = []
+            all_annotations_moments.push(["01:00", "04:00"]);
+            all_annotations_comments.push("My favorite part!");
 
-		all_annotations_moments.push(["01:00", "04:00"]);
-		all_annotations_comments.push("My favorite part!");
+            all_annotations_moments.push(["07:00", "13:00"]);
+            all_annotations_comments.push("It is ok!");
 
-		all_annotations_moments.push(["07:00", "13:00"]);
-		all_annotations_comments.push("It is ok!");
+            add_insert_annotation_button();
 
-		add_insert_annotation_button();
+            var trigger = setInterval(function(){
+                var full_time = document.getElementById("scrubberDuration");
 
-		var trigger = setInterval(function(){ 
-		    var full_time = document.getElementById("scrubberDuration");
-		    
-		    if (full_time != null && full_time.innerHTML != "00:00")
-		    {
-	        	fill_with_annotations(full_time.innerHTML, all_annotations_moments, all_annotations_comments);
-		    	clearInterval(trigger);
-	        }
-		}, 1000);
-	}
+                if (full_time != null && full_time.innerHTML != "00:00")
+                {
+                    fill_with_annotations(full_time.innerHTML, all_annotations_moments, all_annotations_comments);
+                    clearInterval(trigger);
+                }
+            }, 1000);
+        }
 
-	function show_added_annotations_until_now()
-	{
-		var holder = document.getElementById("content");	
-	    var container = insertAnnotator(holder, response.html);
-	   
+        function show_added_annotations_until_now()
+        {
+            var holder = document.getElementById("content");
+            var container = insertAnnotator(holder, dependencies.annotatorPopup);
 
-		container.style.position = "fixed";
-		container.style.top = "30%";
-		container.style.right = "0%";
-		container.style.zIndex = 1000;
-	}
 
-	function add_insert_annotation_button()
-	{
-		var authentification_bar = document.getElementsByClassName("auth-links__container___rSeFg")[0];
-		
-		var new_annotate_button = document.createElement("a");
-		var new_annotate_span = document.createElement("span");
-		var new_annotate_separator = document.createElement("i");
+            container.style.position = "fixed";
+            container.style.top = "30%";
+            container.style.right = "0%";
+            container.style.zIndex = 1000;
+        }
 
-		new_annotate_separator.setAttribute("class", "icons__icon-dot___GHWpv auth-links__divider___1pb-I");
-		authentification_bar.appendChild(new_annotate_separator);
+        function add_insert_annotation_button()
+        {
+            var authentification_bar = document.getElementsByClassName("auth-links__container___rSeFg")[0];
 
-		new_annotate_span.setAttribute("style", "font-family:inherit;");
-		new_annotate_span.innerText = "Add Annotation";
-		new_annotate_button.appendChild(new_annotate_span);
+            var new_annotate_button = document.createElement("a");
+            var new_annotate_span = document.createElement("span");
+            var new_annotate_separator = document.createElement("i");
 
-		new_annotate_button.setAttribute("class", "auth-links__whiteLink___3tT0y auth-links__link___GsWd7 auth-links__baseMuiTransition___1AQHm");
-		new_annotate_button.setAttribute("role", "button");
-		new_annotate_button.setAttribute("id", "add_annotation_button");
-		authentification_bar.appendChild(new_annotate_button);
+            new_annotate_separator.setAttribute("class", "icons__icon-dot___GHWpv auth-links__divider___1pb-I");
+            authentification_bar.appendChild(new_annotate_separator);
 
-		var created_button = document.getElementById("add_annotation_button");
+            new_annotate_span.setAttribute("style", "font-family:inherit;");
+            new_annotate_span.innerText = "Add Annotation";
+            new_annotate_button.appendChild(new_annotate_span);
 
-		created_button.addEventListener("click", function() { 
-			console.log("ADDED"); 
+            new_annotate_button.setAttribute("class", "auth-links__whiteLink___3tT0y auth-links__link___GsWd7 auth-links__baseMuiTransition___1AQHm");
+            new_annotate_button.setAttribute("role", "button");
+            new_annotate_button.setAttribute("id", "add_annotation_button");
+            authentification_bar.appendChild(new_annotate_button);
 
-			document.getElementById("playerActionButton").click();
+            var created_button = document.getElementById("add_annotation_button");
 
-			var current_time_passed = document.getElementById("scrubberElapsed").innerHTML;
-			
-			var holder = document.getElementById("content");	
-		    var container = insertAnnotator(holder, response.html);
-		   
-			container.style.position = "fixed";
-			container.style.bottom = "10%";
-			container.style.left = "25%";
-			container.style.zIndex = 100;
+            created_button.addEventListener("click", function() {
+                console.log("ADDED");
 
-			// removeAnnotator();
-			// document.getElementById("playerActionButton").click();
-		});
-	}
+                document.getElementById("playerActionButton").click();
 
-	function fill_with_annotations(full_time, annotations_moments, annotations_comments)
-	{
-		for (var iterator = 0; iterator < annotations_moments.length; iterator++)
-			add_annotation_to_the_list(annotations_moments[iterator],  annotations_comments[iterator], full_time);
-	}
+                var current_time_passed = document.getElementById("scrubberElapsed").innerHTML;
 
-	function add_annotation_to_the_list(annotation_moment, annotation_comment, full_time)
-	{
-		var full_time_bar = document.getElementById("scrubber").childNodes[0].childNodes[0];
+                var holder = document.getElementById("content");
+                var container = insertAnnotator(holder, dependencies.annotatorPopup);
 
-		var new_annotation = document.createElement("div");
-		new_annotation.setAttribute("id", annotation_comment);
+                container.style.position = "fixed";
+                container.style.bottom = "10%";
+                container.style.left = "25%";
+                container.style.zIndex = 100;
 
-		var new_annotate_start_time_in_seconds = estimate_time_in_seconds(annotation_moment[0])
-		var new_annotate_finish_time_in_seconds = estimate_time_in_seconds(annotation_moment[1])
+                // removeAnnotator();
+                // document.getElementById("playerActionButton").click();
+            });
+        }
 
-		var start_push_pixels = new_annotate_start_time_in_seconds * full_time_bar.offsetWidth / estimate_time_in_seconds(full_time);
-		var final_push_pixels = (new_annotate_finish_time_in_seconds - new_annotate_start_time_in_seconds) / estimate_time_in_seconds(full_time) * 100;
-		var style_for_new_annotate = "position: absolute; height: 100%; transition: margin 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms; left: " + start_push_pixels + "px; background-color: rgb(255, 0, 0); width: calc(" + final_push_pixels +"%); "
-		
-		new_annotation.setAttribute("style", style_for_new_annotate);
-		full_time_bar.appendChild(new_annotation);
+        function fill_with_annotations(full_time, annotations_moments, annotations_comments)
+        {
+            for (var iterator = 0; iterator < annotations_moments.length; iterator++)
+                add_annotation_to_the_list(annotations_moments[iterator],  annotations_comments[iterator], full_time);
+        }
 
-		var created_annotation = document.getElementById(annotation_comment);
-		add_listener_for_annotation(created_annotation, annotation_comment, annotation_moment);
-	}
+        function add_annotation_to_the_list(annotation_moment, annotation_comment, full_time)
+        {
+            var full_time_bar = document.getElementById("scrubber").childNodes[0].childNodes[0];
 
-	function estimate_time_in_seconds(time)
-	{
-		var each_part_of_time = time.split(":")
-		return Number(each_part_of_time[0]) * 60 + Number(each_part_of_time[1])
-	}
+            var new_annotation = document.createElement("div");
+            new_annotation.setAttribute("id", annotation_comment);
 
-	function add_listener_for_annotation(annotation, comment, time_moments)
-	{
-		annotation.addEventListener("mouseover", function() { 
-			console.log("OVER ANNOTATION LINE");
+            var new_annotate_start_time_in_seconds = estimate_time_in_seconds(annotation_moment[0])
+            var new_annotate_finish_time_in_seconds = estimate_time_in_seconds(annotation_moment[1])
 
-			var holder = document.getElementById("content");	
-		    var container = insertAnnotator(holder, response.html);
-		   
-			container.style.position = "fixed";
-			container.style.bottom = "10%";
-			container.style.left = "25%";
-			container.style.zIndex = 100;
+            var start_push_pixels = new_annotate_start_time_in_seconds * full_time_bar.offsetWidth / estimate_time_in_seconds(full_time);
+            var final_push_pixels = (new_annotate_finish_time_in_seconds - new_annotate_start_time_in_seconds) / estimate_time_in_seconds(full_time) * 100;
+            var style_for_new_annotate = "position: absolute; height: 100%; transition: margin 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms; left: " + start_push_pixels + "px; background-color: rgb(255, 0, 0); width: calc(" + final_push_pixels +"%); "
 
-		}); 
+            new_annotation.setAttribute("style", style_for_new_annotate);
+            full_time_bar.appendChild(new_annotation);
 
-		annotation.addEventListener("mouseout", function() { 
-			console.log("OUT ANNOTATION LINE");
+            var created_annotation = document.getElementById(annotation_comment);
+            add_listener_for_annotation(created_annotation, annotation_comment, annotation_moment);
+        }
 
-			removeAnnotator();
-		});
-	}
+        function estimate_time_in_seconds(time)
+        {
+            var each_part_of_time = time.split(":")
+            return Number(each_part_of_time[0]) * 60 + Number(each_part_of_time[1])
+        }
 
-	main_function();
-});
+        function add_listener_for_annotation(annotation, comment, time_moments)
+        {
+            annotation.addEventListener("mouseover", function() {
+                console.log("OVER ANNOTATION LINE");
+
+                var holder = document.getElementById("content");
+                var container = insertAnnotator(holder, dependencies.annotatorPopup);
+
+                container.style.position = "fixed";
+                container.style.bottom = "10%";
+                container.style.left = "25%";
+                container.style.zIndex = 100;
+
+            });
+
+            annotation.addEventListener("mouseout", function() {
+                console.log("OUT ANNOTATION LINE");
+
+                removeAnnotator();
+            });
+        }
+
+        main_function();
+	});
+
