d6768538eb9b7140bbcbcea8bab0ecc4e18ba23b
AlexMihai03 <alexmihai.03@gmail.com>
2017-11-27 19:49:55 +0200

    get dependences and build a foundation

--- src/scripts/content-vimeo.js
+++ src/scripts/content-vimeo.js
@@ -1,1 +1,18 @@
-console.log("Script for vimeo is running");
+let embeddedHTML;
+let annotatorActions;
+
+addAnnotatorActions = function(template) {
+    var target = document.getElementsByClassName("clip-categories")[0];
+
+    console.log(target);
+}
+
+getAllDependencies()
+    .then(dependences => {
+        embeddedHTML = dependences.annotatorPopup;
+        annotatorActions = dependences.annotationActionsMenu;
+
+        //addDotOnProgressBar();
+        addAnnotatorActions(annotatorActions);
+    });
+
