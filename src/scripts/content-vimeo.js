0f96be6143d17f7b300bc1765a4ddb925bbdec69
Mihai Alexandru <alexmihai.03@gmail.com>
2018-01-12 12:49:15 +0200

    working on annotationButton on vimeo

--- src/scripts/content-vimeo.js
+++ src/scripts/content-vimeo.js
@@ -12,30 +12,25 @@ addAnnotatorActions = function(template) {
     
 }
 
+insertAnnotationButton = function() {
+    var sidedock = document.getElementsByClassName("sidedock")[0];
+    var annotationButton = sidedock.lastChild
+                            .cloneNode(true);
+
+    annotationButton.firstChild.firstChild.textContent = "Add Annotation";
+    
+    sidedock.appendChild(annotationButton);
+
+    console.log(annotationButton);
+    
+}
+
 getAllDependencies()
     .then(dependences => {
         embeddedHTML = dependences.annotatorPopup;
         annotatorActions = dependences.annotationActionsMenu;
 
+        insertAnnotationButton();
         //addDotOnProgressBar();
-        addAnnotatorActions(annotatorActions);
+        //addAnnotatorActions(annotatorActions);
     });
-
-    storage: {
-        annotations: [
-          {
-            id: "1",
-            site: "https://www.youtube.com/watch?v=x-sbLkkEuwg",
-            timestamp: "03-12-2017::18:48:34",
-            startTime: "12:02",
-            endTime: "12:33",
-            keywords: ["music", "album", "JaymesYoung"],
-            content: "<h1>Second song</h1><br><img>$1</img><br><img>$2</img><br>",
-            images: ["1", "2"] // array with the images in annotation
-          }],
-        tags: ["album", "music", "programming","podcast"],
-        noImages: "2", // number of images
-        images1: [{id: "1", bytes: []}, {id: "2", bytes: []}], // images with id between 1 and 10
-        favourites: ["1"] // array with the id of an annotation
-        folder1: {id: "1", name: "Programming", annotations: ["2", "3"]}
-      }
